14c2e943659c4da6fe1b2a5e8467aad504523067
yihsuan123 <yihsuan@linkernetworks.com>
2024-05-21 09:41:57 +0800

    chore(python): udpate version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 AUTHOR = "LinkerVision"
 PACKAGE_NAME = "visionai-data-format"
-PACKAGE_VERSION = "1.3.3"
+PACKAGE_VERSION = "1.3.4"
 DESC = "converter tool for visionai format"
 REQUIRED = ["pydantic==1.*", "pillow"]
 REQUIRES_PYTHON = ">=3.7, <4"
